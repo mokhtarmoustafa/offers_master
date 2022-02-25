57c4c540f81ab8ea4fb3c0da78ea36a1df986c41
mokhtarmoustafa <mokhtar.rnd@gmail.com>
2022-02-25 07:20:47 +0200

    Splash Screen - Create Design https://mokhtar10.atlassian.net/browse/OF-1

---
+++ app/src/main/java/com/twoam/offers/ui/SplashFragment.kt
@@ -0,0 +1,39 @@
+package com.twoam.offers.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.twoam.offers.R
+
+class SplashFragment : Fragment() {
+
+    //region variables
+    val TIMER_DURATION=1000L
+    //endregion
+    //region events
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+
+    //endregion
+
+    //region helper functions
+
+    //endregion
+    companion object {
+        private const val TAG = "SplashFragment"
+    }
+}
